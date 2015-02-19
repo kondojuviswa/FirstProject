787c1bfb9cf7fee150d2bf72f1f8fe324dd74fb2
Viswanadha Pratap <kondojuviswanadha@gmail.com>
2015-02-19 00:05:03 -0600

    19-02-2015

--- src/impDSandALGO/LinkedListPractice.java
+++ src/impDSandALGO/LinkedListPractice.java
@@ -2,66 +2,128 @@
  * 
  */
 package impDSandALGO;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.*;
 
 /**
  * @author My PC
  *
  */
-public class LinkedListPractice extends LinkedList<Integer> implements SortedSet<Integer>{
+public class LinkedListPractice {
 
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
+	
+	/* doUnion will computer the union of the given linkedlist
+	 * and return the combined List of the elements between these
+	 * two lists
+	 */
+	LinkedList<Integer> doUnion(LinkedList<Integer> l1,LinkedList<Integer> l2)
+	{
+		LinkedList<Integer> combinedList = new LinkedList<Integer>();
+		while(!l1.isEmpty()&& !l2.isEmpty())
+		{
+			
+			if(l1.peekFirst() < l2.peekFirst())
+				combinedList.add(l1.remove());
+			else if(l1.peekFirst() > l2.peekFirst())
+				combinedList.add(l2.remove());  
+			else 
+			{
+				combinedList.add(l1.remove());
+				l2.remove();
+			}
+		
+		}
+		if(l1.isEmpty()&& !l2.isEmpty())
+		{
+			combinedList.addAll(l2);
+		}
+		else if(!l1.isEmpty() && l2.isEmpty())
+		{
+			combinedList.addAll(l1);
+		}
+	
+	return combinedList;
+	}
+	
+	/* doIntersection will compute the intersection of the two linked lists
+	 * and return the common list of elements between the two lists
+	 */
+	LinkedList<Integer> doIntersection(LinkedList<Integer> l1 ,LinkedList<Integer> l2)
+	{
+		LinkedList<Integer> commonList = new LinkedList<Integer>();
+		while(!l1.isEmpty() && !l2.isEmpty())
+		{
+			if(l1.peekFirst() == l2.peekFirst())
+			{
+				commonList.add(l1.remove());
+				l2.remove();
+			}
+			else if(l1.peekFirst() < l2.peekFirst())
+			{
+				l1.remove();
+			}
+			else
+			{
+				l2.remove();
+			}
+		}
+		return commonList;
+	}
+	public static void main(String[] args) throws FileNotFoundException{
 		// TODO Auto-generated method stub
-		LinkedListPractice ll = new LinkedListPractice();
-		ll.add(1);
-		ll.add(5);
-		ll.add(6);
-		ll.add(2);
+		LinkedList<Integer> l1 = new LinkedList<Integer>();
+		LinkedList<Integer> l2 = new LinkedList<Integer>();
+		Scanner in;
+		 if (args.length > 0) {
+	            File inputFile = new File(args[0]);
+	            in = new Scanner(inputFile);
+	           // System.out.println(in.toString());
+
+	        } else {
+	           in = new Scanner(System.in);
+	       }
+		 while(in.hasNextLine())
+		 {
+			 System.out.println(in.next());
+		 }
+		 
+
 		
-		System.out.println(ll);
+		int i = 0;
+		while(i<7)
+		{
+			l1.add(i++*2);
+		}
+	//	System.out.println(l1);
+		 i = 0;
+		while(i < 5)
+		{
+			l2.add(i++*3);
+		}
+	//	System.out.println(l2);
+		LinkedListPractice llp = new LinkedListPractice();
+		LinkedList<Integer> l3 = new LinkedList<Integer>();
+		LinkedList<Integer> l4 = new LinkedList<Integer>();
+		//l3 = llp.doUnion(l1, l2);
+		l4 = llp.doIntersection(l1, l2);
 		
+		//System.out.println("Union of the above two lists is:" + l3);
+	//	System.out.println("Intersection of the above two lists is:" + l4);
+		
+	
+		/*l3.add(5);
+		l3.addAll(l1);
+		System.out.println(l3);
+		l3.remove();
+		System.out.println(l3);*/
 		
 		
  
 	}
 
-	@Override
-	public Comparator<? super Integer> comparator() {
-		// TODO Auto-generated method stub
-		return null;
+	//
 	}
-
-	@Override
-	public SortedSet<Integer> subSet(Integer fromElement, Integer toElement) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public SortedSet<Integer> headSet(Integer toElement) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public SortedSet<Integer> tailSet(Integer fromElement) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Integer first() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Integer last() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-}
